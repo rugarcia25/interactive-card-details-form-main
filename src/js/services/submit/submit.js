c6e9a09f91c538399beaf9b6a89e01f77cce9677
Bruno Moleta <brunomoleta@pm.me>
2024-04-25 18:43:43 -0300

    ✨ Insert person's name
    After submitting the Form, the first name of the cardholder is displayed at the screen.

--- src/js/services/submit/submit.js
+++ src/js/services/submit/submit.js
@@ -1,7 +1,13 @@
-const form= document.querySelector('form');
-const section= document.querySelector('#section-hidden');
+const form = document.querySelector('form');
+const cardName = document.querySelector('#card-name');
+const giveMessage = document.querySelector('#thanks');
+
+const section = document.querySelector('#section-hidden');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+
   form.classList.add('hidden');
   section.classList.remove('hidden');
+
+  giveMessage.textContent = 'Thank you, ' + cardName.innerText.split(' ')[0];
 });
